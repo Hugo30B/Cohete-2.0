7d1a8452e08cb9735fb3e958e839133cf9834c1c
robokt <45914339+robokt@users.noreply.github.com>
2018-12-16 12:12:20 +0100

    Add files via upload

---
+++ roket_1.ino
@@ -0,0 +1,18 @@
+#include <Servo.h>
+Servo myServo;
+int switchState = 0;
+void setup() {
+  myServo.attach(10);
+  myServo.write(90);
+  pinMode(5,INPUT);
+  pinMode(4,OUTPUT);
+}
+void loop() {  
+     switchState = digitalRead(5);
+      if (switchState == LOW){
+        myServo.write(180); 
+ }
+ else {    
+   digitalWrite(4,LOW);
+ }
+}
