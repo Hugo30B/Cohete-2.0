08f8792d4dbabbd8e4195e628762c2bb9b4202bc
robokt <45914339+robokt@users.noreply.github.com>
2019-07-29 17:45:42 +0200

    Update roket_1.ino

--- Cohete-2.0-master/Cohete-2.0-master/Programaciones/roket_1/roket_1.ino
+++ Cohete-2.0-master/Cohete-2.0-master/Programaciones/roket_1/roket_1.ino
@@ -4,15 +4,8 @@ int switchState = 0;
 void setup() {
   myServo.attach(10);
   myServo.write(90);
-  pinMode(5,INPUT);
-  pinMode(4,OUTPUT);
 }
 void loop() {  
-     switchState = digitalRead(5);
-      if (switchState == LOW){
-        myServo.write(180); 
- }
- else {    
-   digitalWrite(4,LOW);
- }
+delay(1500);
+myServo.write(1)
 }
